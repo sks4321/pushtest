e109305d130a9c6d1f5f683cf2026ea06d23f214
Administrator <Administrator@smart20113>
2017-05-31 15:14:34 +0900

    dbproperitesInit 통한 DB 연결과 selectMemberById 메소드 선언

--- jdbcapp/src/jdbcapp/MemberDao.java
+++ jdbcapp/src/jdbcapp/MemberDao.java
@@ -1,8 +1,58 @@
 package jdbcapp;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Properties;
+
+
 public class MemberDao {
-	public Member selectMemberById(String id ){
-		// properties db 정보를 가져올 거에요(내부적으로는 input output 쓸꺼에요)
-		return null;
+	private String url;//외부파일에 둘겂이다.
+	private String dbid;//외부파일에 둘겂이다.
+	private String dbpw;//외부파일에 둘겂이다.
+	private void dbpropertiesInit() throws IOException{
+		FileInputStream fis = new FileInputStream("d:\\db.properties");
+		Properties pro = new Properties(); // ? = ? 이런식으로 만들어져 있는 파일을 읽기 쉽다.
+		pro.load(fis);// 어트리뷰트로  바뀜
+		this.url = pro.getProperty("url");
+		this.dbid = pro.getProperty("dbid");
+		this.dbpw = pro.getProperty("dbpw");
+	}
+	public Member selectMemberbyID(String id) throws ClassNotFoundException, SQLException, IOException{
+		this.dbpropertiesInit();
+		System.out.println(this.dbid);
+		System.out.println(this.dbpw);
+		//Properties DB정보 가져옵니다(내부적으로 input...
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection conn = DriverManager.getConnection(this.url, this.dbid, this.dbpw);
+		String sql = "SELECT * FROM oracle_member WHERE ora_id = ?";
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		ResultSet rs = pstmt.executeQuery();
+		Member member = null;
+		if(rs.next()){
+			member = new Member();
+			member.setOra_id(rs.getString("ora_id"));
+			member.setOra_pw(rs.getString("ora_pw"));
+			member.setOra_level(rs.getString("ora_level"));
+			member.setOra_name(rs.getString("ora_name"));
+			member.setOra_email(rs.getString("ora_email"));
+		}
+		return member;
+	}
+	//유니테스트 함.
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException{
+		MemberDao mda = new MemberDao();
+		Member m = mda.selectMemberbyID("id001");
+		System.out.println(m.getOra_id().equals("id001"));
+		System.out.println(m.getOra_pw());
+		System.out.println(m.getOra_level());
+		System.out.println(m.getOra_name());
+		System.out.println(m.getOra_email());
+
 	}
 }
