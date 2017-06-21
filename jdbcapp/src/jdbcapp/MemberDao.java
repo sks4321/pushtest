81911700282eb87eb36aff4a5157ae749ad9f456
Administrator <Administrator@smart20113>
2017-06-21 13:49:13 +0900

    주석 테스트 2

--- jdbcapp/src/jdbcapp/MemberDao.java
+++ jdbcapp/src/jdbcapp/MemberDao.java
@@ -9,6 +9,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Properties;
 // 주석을 작성해서 test 해보는 부분
+// 주석을 작성해서 test 해보는 부분 1
 public class MemberDao {
 	private String url;//외부파일에 둘겂이다.
 	private String dbid;//외부파일에 둘겂이다.
